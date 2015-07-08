d3a5347aff950a5ad347ba8fae6b6d16e857a7fe
g.verdi <verdi1813@users.noreplay.github.com>
2015-07-08 17:12:05 +0200

    Con página del autor

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET /author */
+router.get('/author', function(req, res) {
+  res.render('author', { autor: 'Verdi1813', foto: 'verdi1813.jpg' });
+});
+
 router.get('/quizes/question',  quizController.question);
 router.get('/quizes/answer',    quizController.answer);
 
