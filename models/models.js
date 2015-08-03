859aa406ad1b3ec1abda7dba78b3719bc90ca4e3
g.verdi <verdi1813@users.noreplay.github.com>
2015-08-03 10:07:07 +0200

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -27,10 +27,18 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 );
 
 // Importar definicion de la tabla Quiz
-var quiz_path = path.join(__dirname,'quiz');
+var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; // exportar tabla Quiz
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz;        // exportar tabla Quiz
+exports.Comment = Comment;  // exportar tabla Comment
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
